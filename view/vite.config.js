6a1cd4ba553edc18e57734b026181aaa50d28b30
Paris Hoffman <47543650+parishoffman@users.noreply.github.com>
2025-03-01 12:06:05 -0800

    changed vite.config file again

--- view/vite.config.js
+++ view/vite.config.js
@@ -3,6 +3,11 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server: {allowedHosts: true},
   plugins: [react()],
+  server: {
+    allowedHosts: [
+      'localhost',
+      'ec2-18-232-111-74.compute-1.amazonaws.com'
+    ]
+  }
 })
