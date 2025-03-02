dd314baf881a9830bc1cb2981816db7c1b0aec79
Paris Hoffman <47543650+parishoffman@users.noreply.github.com>
2025-03-01 16:01:10 -0800

    changed baseURL in http.js

--- view/src/http.js
+++ view/src/http.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 // Base URL should point to the API root, not a specific endpoint
 let http = axios.create({
-  baseURL: 'http://localhost:8080/api',
+  baseURL: '/api',
   headers: {
     'Content-type': 'application/json'
   },
